3d91f6a56fb2c892eb16e955deb17caec627a834
andreiciorceanu <101288885+andreiciorceanu@users.noreply.github.com>
2023-04-06 16:35:09 +0300

    Colocviu subiect B.1,B.2 si C.1

--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var06/PracticalTest01Var06MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var06/PracticalTest01Var06MainActivity.java
@@ -2,11 +2,14 @@ package ro.pub.cs.systems.eim.practicaltest01var06;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -15,7 +18,10 @@ public class PracticalTest01Var06MainActivity extends AppCompatActivity {
     private Button play;
     private EditText text1, text2, text3;
     private CheckBox check1,check2,check3;
-    String number1, number2, number3;
+    String generatedNumber1, generatedNumber2, generatedNumber3;
+    String a,b,c;
+    private int nrCheckedBoxes = 3;
+    private int scor = 0;
     private StringBuilder sb = new StringBuilder();
 
     @Override
@@ -23,17 +29,69 @@ public class PracticalTest01Var06MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test01_var06_main);
 
+        play = findViewById(R.id.btn_invoke_activity);
         text1 = findViewById(R.id.editText1);
         text2 = findViewById(R.id.editText2);
         text3 = findViewById(R.id.editText3);
-        play = findViewById(R.id.btn_invoke_activity);
+        check1 = findViewById(R.id.checkBox1);
+        check2 = findViewById(R.id.checkBox2);
+        check3 = findViewById(R.id.checkBox3);
 
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                nrCheckedBoxes = 3;
+                generatedNumber1 = generateNumber();
+                generatedNumber2 = generateNumber();
+                generatedNumber3 = generateNumber();
+                if(!check1.isChecked()) {
+                    text1.setText(generatedNumber1);
+                    nrCheckedBoxes--;
+                    a = generatedNumber1;
+                }
+                if(!check2.isChecked()) {
+                    text2.setText(generatedNumber2);
+                    nrCheckedBoxes--;
+                    b = generatedNumber2;
+                }
+                if(!check3.isChecked()) {
+                    text3.setText(generatedNumber3);
+                    nrCheckedBoxes--;
+                    c = generatedNumber3;
+                }
 
+                String message = "Generated numbers: " + generatedNumber1 + ", " + generatedNumber2 + ", " + generatedNumber3;
+                Toast.makeText(PracticalTest01Var06MainActivity.this, message, Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(PracticalTest01Var06MainActivity.this, PracticalTest01Var06SecondaryActivity.class);
+                intent.putExtra("number1", a);
+                intent.putExtra("number2",b);
+                intent.putExtra("number3", c);
+                intent.putExtra("boxes", nrCheckedBoxes);
+                startActivityForResult(intent, 1);
             }
         });
+
+        if(savedInstanceState != null) {
+            if(savedInstanceState.containsKey("scor")) {
+                scor = savedInstanceState.getInt("scor");
+            } else {
+                scor = 0;
+            }
+        } else {
+            scor = 0;
+        }
+        //Toast.makeText(this, "The result of the operation is " + scor, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
+            int scorPrimit = data.getIntExtra("scor", 0);
+            scor += scorPrimit;
+            Toast.makeText(this, "The result of the operation is " + scor, Toast.LENGTH_SHORT).show();
+        }
     }
 
     private String generateNumber() {
--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var06/PracticalTest01Var06SecondaryActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var06/PracticalTest01Var06SecondaryActivity.java
@@ -2,13 +2,97 @@ package ro.pub.cs.systems.eim.practicaltest01var06;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.TestLooperManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class PracticalTest01Var06SecondaryActivity extends AppCompatActivity {
-
+    private TextView afisaj;
+    private Button ok;
+    private StringBuilder sb = new StringBuilder();
+    private int scor = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test01_var06_secondary);
+
+        afisaj = findViewById(R.id.gained);
+        ok = findViewById(R.id.ok);
+
+        Bundle extras = getIntent().getExtras();
+
+        // Get the terms as a string
+        String number1 = extras.getString("number1");
+        String number2 = extras.getString("number2");
+        String number3 = extras.getString("number3");
+        int nrBoxes = extras.getInt("boxes", 0);
+
+        if(number1.equals("*")) {
+            if(number2.equals(number3) || number2.equals("*") || number3.equals("*")) {
+                sb.append("Gained");
+                scor = calculeazaCastig(nrBoxes);
+                sb.append(" ");
+                sb.append(scor);
+                afisaj.setText(sb.toString());
+            }
+        } else {
+            if(number2.equals("*")) {
+                if(number1.equals(number3) || number3.equals("*")) {
+                    sb.append("Gained");
+                    scor = calculeazaCastig(nrBoxes);
+                    sb.append(" ");
+                    sb.append(scor);
+                    afisaj.setText(sb.toString());
+                }
+            } else {
+                if(number3.equals("*")) {
+                    if(number1.equals(number2)) {
+                        sb.append("Gained");
+                        scor = calculeazaCastig(nrBoxes);
+                        sb.append(" ");
+                        sb.append(scor);
+                        afisaj.setText(sb.toString());
+                    }
+                } else {
+                    if(number1.equals(number2) && number2.equals(number3)) {
+                        sb.append("Gained");
+                        scor = calculeazaCastig(nrBoxes);
+                        sb.append(" ");
+                        sb.append(scor);
+                        afisaj.setText(sb.toString());
+                    }
+                }
+            }
+        }
+
+
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sb.setLength(0);
+                Intent intent = new Intent();
+                intent.putExtra("scor",scor);
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
     }
+
+    private int calculeazaCastig(int nrBife) {
+        switch (nrBife) {
+            case 0:
+                return 100;
+            case 1:
+                return 50;
+            case 2:
+                return 10;
+            default:
+                return 0;
+        }
+    }
+
 }
